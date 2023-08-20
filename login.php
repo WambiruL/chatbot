1e0dd9e59198610fc93fe654119568ebc8b857b4
WambiruL <lwambui2002@gmail.com>
2023-08-21 00:36:43 +0300

    Designed the signup and login pages UI

--- login.php
+++ login.php
@@ -1,27 +1,46 @@
 <?php
 session_start();
 require "dbconfig/config.php";
+require_once 'header.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
     <title>Login</title>
 </head>
-<body>
-    <form class="my_form" action="login.php" method="post">
-        <label><b id="login_name">Username:</b></label><br>
-        <input name="username" id="us" type="text" placeholder="Enter Username Here..."><br>
-        <label><b id="login_pass">Password:</b></label><br>
-        <input name="password" id="pass" type="password" placeholder="Your Password"><br>
-        <input name="login" type="submit" id="login_btn" value="Login"><br>
-        -OR-<br>
-        <a href="register.php"><input type="button" id="register_btn" value="Register"><br></a>
-        <a href="admin.php"><input type="button" id="admin_btn" value="Login as Admin"></a>    
-    </form>
+<body style="overflow-y:hidden;">
+    <section class="vh-100 gradient-custom" style="background-image:url('images/signup3.jpg');">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card text-white" style="border-radius: 1rem; background-color:white;">
+                        <div class="card-body p-5 text-center">
+                            <div class="mb-md-5 mt-md-4 pb-5">
+                                <h2 class="fw-bold mb-2 text-uppercase" style="color:black;">Login</h2>
+                                <p class="text-black-50 mb-5">Please enter your Username and Password!</p>
 
-    <?php
+                                <form action="login.php" method="post">
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fa fa-user fa-lg me-3 fa-fw" style="color:black;"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input type="text" name="username" id="form3Example1c" class="form-control" placeholder="Your Name" />                                                
+                                         </div>
+                                    </div>
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fa fa-lock fa-lg me-3 fa-fw" style="color:black;"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input type="password" name="password" id="form3Example4c" class="form-control" placeholder="Password"/>                                                
+                                        </div>
+                                    </div>
+
+                                    <input name="login" class="btn btn-outline-light btn-lg px-5" type="submit">                 
+                                </form><br>
+                                <?php
     if(isset($_POST['login'])){
         $username=htmlspecialchars($_POST['username']);
         $password=htmlspecialchars($_POST['password']);
@@ -40,6 +59,18 @@ require "dbconfig/config.php";
         }
     }
     ?>
+                                <a href="admin.php"><input type="button" class="btn btn-outline-dark px-5" id="admin_btn" value="Login as Admin"></a>
+                            </div>
+                        <div>
+                            <p class="mb-0 text-black-50">Don't have an account? <a href="register.php" class="text-black-50 fw-bold">Sign Up</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>   
+
+    
     
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,73 +1,129 @@
 <?php
 require_once 'dbconfig/config.php';
+require_once 'header.php';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+
     <title>Register</title>
 </head>
 <body>
-    <h2>Sign Up</h2>
-    <form class="my_form" action="register.php" method="post">
-        <label><b id="name">Username:</b></label><br>
-        <input name="username" type="text" id="ruser" placeholder="Username" required><br>
-        <label><b id="pass">Password:</b></label><br>
-        <input name="password" type="password" id="password" placeholder="Password" required><br>
-        <label><b id="pass2">Confirm Password:</b></label><br>
-        <input name="password2" type="password" id="password2" placeholder="Confirm Password" required><br>
-        <label><b id="email">Email:</b></label><br>
-        <input name="email" type="email" id="email" placeholder="Email" required><br>
-        <input name="submit_btn" type="submit" id="signup_btn" value="Sign Up"><br>
-        <a href="index.php"><input type="button" id="back_btn" value="Back"></a>
-    </form>
+    <section class="vh-100" style="background-image:url('images/signup3.jpg');">  
+        <div class="container  h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-lg-12 col-xl-11">
+                    <div class="card text-black" style="border-radius: 25px;">
+                        <div class="card-body p-md-5">
+                            <div class="row justify-content-center">
+                                <div class="col-md-8 col-lg-6 col-xl-5 order-2 order-lg-1">                                         
+                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p> 
 
-    <?php
-        if(isset($_POST['submit_btn'])){
-            $username=htmlspecialchars($_POST['username']);
-            $password=htmlspecialchars($_POST['password']);
-            $password2=htmlspecialchars($_POST['password2']);
-            $email=htmlspecialchars($_POST['email']);
+                                    <form class="mx-1 mx-md-4" action="register.php" method="post">
 
-            if($password==$password2){
-                $query="SELECT * FROM users WHERE username=:username;";
-                $stmt=$pdo->prepare($query);
-                $stmt->bindparam(":username", $username);
-                $stmt->execute();
-                if($stmt->rowCount()>0){
-                    echo '<script type="text/javascript"> alert("User already exists. Try another Username")</script>';
-                    $pdo=null;
-                    $stmt=null;
-                    die();
-                }else{
-                    $query="INSERT INTO users(username,password,email) VALUES(:username,:password,:email);";
-                    $stmt=$pdo->prepare($query);
-                    $options=[
-                        'cost'=>12
-                    ];
-                    $hashedPwd=password_hash($password,PASSWORD_BCRYPT, $options);
-                    $stmt->bindparam(":username",$username);
-                    $stmt->bindparam(":password",$hashedPwd);
-                    $stmt->bindparam(":email",$email);
-                    if($stmt->execute()){
-                        echo'<script type="text/javascript">alert("User Registered. Go to the login page to login")</script>';
-                        header("Location:login.php?signup=success");
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fa fa-user fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="text" name="username" id="form3Example1c" class="form-control" placeholder="Your Name" />                                                
+                                            </div>
+                                        </div>
 
-                    }else{
-                        echo '<script type="text/javascript">alert("throw new PDOException($e->getMessage())")</script>';
-                        
-                    }
-                    $pdo=null;
-                    $stmt=null;
-                    die();
-                }
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fa fa-lock fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="password" name="password" id="form3Example4c" class="form-control" placeholder="Password"/>                                                
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fa fa-key fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="password"  name="password2" id="form3Example4cd" class="form-control" placeholder="Confirm Password" />
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fa fa-envelope fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="email" name="email" id="form3Example3c" class="form-control" placeholder="Your Email" />
+                                            </div>
+                                        </div>
+
+                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                            <input type="submit" name="submit_btn" class="btn btn-lg"><br>
+                                            <a href="login.php"><input type="button" class="btn btn-lg" id="back_btn" value="Back"/></a>
+                                        </div>
+           
+                                    </form>
+                                </div>
+                                <div class="col-md-4 d-flex align-items-center order-1 order-lg-2">
+                                    <img src="images/signup4.jpg"  class="img-fluid" alt="Sample image" value="Register">
+                                </div>
+                                <?php
+                                    if(isset($_POST['submit_btn'])){
+                                        $username=htmlspecialchars($_POST['username']);
+                                        $password=htmlspecialchars($_POST['password']);
+                                        $password2=htmlspecialchars($_POST['password2']);
+                                        $email=htmlspecialchars($_POST['email']);
+
+                                        if(empty($username) || empty($password) || empty($password2) || empty($email)){
+                                            echo '<script type="text/javascript"> alert("Fill in all the fields!")</script>';
+                                        }else{                                                               
+                                            if($password==$password2){
+                                                $query="SELECT * FROM users WHERE username=:username;";
+                                                $stmt=$pdo->prepare($query);
+                                                $stmt->bindparam(":username", $username);
+                                                $stmt->execute();
+                                                if($stmt->rowCount()>0){
+                                                    echo '<script type="text/javascript"> alert("User already exists. Try another Username")</script>';
+                                                    $pdo=null;
+                                                    $stmt=null;
+                                                    die();
+                                                }else{
+                                                    $query="INSERT INTO users(username,password,email) VALUES(:username,:password,:email);";
+                                                    $stmt=$pdo->prepare($query);
+                                                    $options=[
+                                                        'cost'=>12
+                                                    ];
+                                                    $hashedPwd=password_hash($password,PASSWORD_BCRYPT, $options);
+                                                    $stmt->bindparam(":username",$username);
+                                                    $stmt->bindparam(":password",$hashedPwd);
+                                                    $stmt->bindparam(":email",$email);
+                                                    if($stmt->execute()){
+                                                        echo'<script type="text/javascript">alert("User Registered. Go to the login page to login")</script>';
+                                                        header("Location:login.php?signup=success");
+
+                                                    }else{
+                                                        echo '<script type="text/javascript">alert("throw new PDOException($e->getMessage())")</script>';
+                                                        $pdo=null;
+                                                        $stmt=null;
+                                                        die();                        
+                                                    }
+                                                
+                                                }
         
-            }else{
-                echo '<script type="text/javascript"> alert("Passwords does not match")</script>';
-            }    
-        }
-    ?>
+                                            }else{
+                                                echo '<script type="text/javascript"> alert("Passwords does not match")</script>';
+                                            }    
+                                        }
+                                    }    
+                                ?>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+
+    
     
 </body>
 </html>
